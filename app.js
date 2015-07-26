e3344f11991ae56f6f06d1558f344ef2057b8c46
Zack Tollman <tollmanz@gmail.com>
2015-07-25 21:11:50 -0700

    Add reports ID route.

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 var Hapi = require('hapi');
 var Glue = require('glue');
 var reportsRoute = require('./lib/routes/reports');
+var reportsIdRoute = require('./lib/routes/reports-id');
 
 Glue.compose(require('./config/manifest.json'), function (err, server) {
   server.route(reportsRoute);
+  server.route(reportsIdRoute);
 
   // Don't start the server when testing
   if (!module.parent) {
