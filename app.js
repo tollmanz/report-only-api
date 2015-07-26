2aba97e88c6cad86b2111d6ed70bee6469d08235
Zack Tollman <tollmanz@gmail.com>
2015-07-25 21:05:43 -0700

    Set architecture for routing.

--- app.js
+++ app.js
@@ -2,7 +2,13 @@
 
 var Hapi = require('hapi');
 var Glue = require('glue');
+var reportsRoute = require('./lib/routes/reports');
 
 Glue.compose(require('./config/manifest.json'), function (err, server) {
-  server.start(function (err) {});
+  server.route(reportsRoute);
+
+  // Don't start the server when testing
+  if (!module.parent) {
+    server.start(function (err) {});
+  }
 });
